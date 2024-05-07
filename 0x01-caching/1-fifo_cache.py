b2cb7c71f47bfce060e7c7e004a1a1c390427fdd
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-07 16:06:00 +0100

    1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -5,6 +5,7 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache (BaseCaching):
+    """BasicCache inherits from BaseCaching and is a caching system"""
     def __init__(self):
         """Initialize BasicCache"""
         super().__init__()
