1d48a5dc22e0c483f94679999d77d9963b39ee04
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-07 17:55:48 +0100

    2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -15,12 +15,11 @@ class LIFOCache (BaseCaching):
         if key is None or item is None:
             return
 
-        self.cache_data[key] = item
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             last_key = next(reversed(self.cache_data))
             print(f'DISCARD: {last_key}')
             del self.cache_data[last_key]
-        
+        self.cache_data[key] = item
 
     def get(self, key):
         """Get an item by key"""
