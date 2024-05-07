6f1e6c8a370948dcc242839dc73061ef31010bfe
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-07 17:51:03 +0100

    2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -14,11 +14,12 @@ class LIFOCache (BaseCaching):
         """Add an item in the cache"""
         if key is None or item is None:
             return
-        self.cache_data[key] = item
+
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            last_key = list(self.cache_data.keys())[-1]
+            last_key = next(reversed(self.cache_data))
             print(f'DISCARD: {last_key}')
             del self.cache_data[last_key]
+        self.cache_data[key] = item
 
     def get(self, key):
         """Get an item by key"""
