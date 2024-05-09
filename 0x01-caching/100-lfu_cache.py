e0e8a8845d12ce2fd447c0bc870713e02846a058
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-09 12:00:22 +0100

    100

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""Implementation of a Least Frequently Used (LFU) cache."""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """
+    Inherits from BaseCaching
+    """
+
+    used = {}
+
+    def __init__(self):
+        """
+        Initializes the LFU Cache
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.used[key] += 1
+            self.cache_data[key] = item
+            return
+        keys = list(self.cache_data.keys())
+        if len(keys) >= BaseCaching.MAX_ITEMS:
+            least = self.LFU_key()
+            print("DISCARD: {}".format(least))
+            del self.cache_data[least]
+            del self.used[least]
+        self.used[key] = 0
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key from the cache
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.used[key] += 1
+        return self.cache_data[key]
+
+    def LFU_key(self):
+        """
+        Find the least frequancy used key
+        """
+        least = -1
+        for key, val in self.used.items():
+            if least == -1 or self.used[least] > val:
+                least = key
+        return least
