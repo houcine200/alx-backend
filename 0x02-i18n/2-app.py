b74f9b3da51636f181070f6d2e2d8d90a78d25fb
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-14 20:18:06 +0100

    2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -16,9 +16,11 @@ class Config():
 
 app.config.from_object(Config)
 
+
 @babel.localeselector
 def get_locale():
-    return request.accept_languages.best_match(["en", "fr"])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index():
