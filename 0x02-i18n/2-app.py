d2e5f04ba74770ef7d3509b7808a8511f6bf0aef
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-14 20:44:02 +0100

    2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -18,7 +18,8 @@ app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
+    '''Get the preferred language based on the user's request'''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
