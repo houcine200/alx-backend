dd068d3bd3cbc967f8f5a2ed4cc2f809018d79cb
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-14 20:11:12 +0100

    2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''Basic Flask application'''
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    '''Configuration class for the application'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(["en", "fr"])
+
+@app.route('/')
+def index():
+    '''Returns the rendered template for index.html page'''
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
