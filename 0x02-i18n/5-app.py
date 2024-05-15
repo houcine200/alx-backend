91ad45090703cfe008ac5c676b8bb10fcc430277
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-15 16:16:04 +0100

    5

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+'''Basic Flask application'''
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    '''Configuration class for the application'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> dict:
+    '''Return user dictionary based on user ID'''
+    id = request.args.get('login_as')
+    if id and int(id) in users:
+        return users.get(int(id))
+    return None
+
+
+def before_request():
+    '''Set user global on flask.g'''
+    g.user = get_user()
+
+
+
+@babel.localeselector
+def get_locale() -> str:
+    '''Get the preferred language based on the user's request'''
+    locale = request.args.get('locale')
+    print(locale)
+    if locale in app.config['LANGUAGES']:
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    '''Returns the rendered template for index.html page'''
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
