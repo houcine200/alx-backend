2eb6594338df3ec98242ada8e8ac31c81960dd95
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-15 16:19:57 +0100

    5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -53,7 +53,7 @@ def get_locale() -> str:
 @app.route('/')
 def index() -> str:
     '''Returns the rendered template for index.html page'''
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
